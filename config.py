3944e983e677d0f639cb11c3dfa35980ef76bebe
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-14 13:51:29 +0700

    _

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
