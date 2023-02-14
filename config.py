df097d2a03549aa21223251fa1c862532d9983e3
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-14 13:42:49 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv("randy.env")
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
