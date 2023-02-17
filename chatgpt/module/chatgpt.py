df6dfdc65012d58756a00702a12ab8679afada9a
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-18 03:57:13 +0700

    add bot pm private `filters.private`

--- chatgpt/module/chatgpt.py
+++ chatgpt/module/chatgpt.py
@@ -18,7 +18,7 @@ from pyrogram.errors import MessageNotModified
 from chatgpt.module.what import *
 from config import OPENAI_API 
 
-@ren.on_message(filters.command("ask") & filters.group)
+@ren.on_message(filters.command("ask") & filters.private | filters.group)
 async def chatgpt(c: Client, m: Message):
     if len(m.command) == 1:
         return await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
@@ -29,12 +29,17 @@ async def chatgpt(c: Client, m: Message):
     }
 
     json_data = {
-        "model": "text-davinci-003",
         "prompt": randydev,
-        "max_tokens": 200,
-        "temperature": 0,
+        "model": "text-davinci-003",
+        "temperature": 0.5,
+        "max_tokens": 1024,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.9,
+        "top_p": 0.3,
+        "frequency_penalty": 0.5,
     }
-    ran = await m.reply("Wait a moment looking for your answer..")
+    ran = await m.reply("Wait a moment looking for your answer..", quote=True)
     try:
         response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
         await ran.edit(response["choices"][0]["text"])
