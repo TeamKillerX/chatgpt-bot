07fb30ffce4988e798ed23732967415adb447f11
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-30 10:02:58 +0700

    `2` seconds  // added handler `!` or `/`

--- chatgpt/module/chatgpt.py
+++ chatgpt/module/chatgpt.py
@@ -19,16 +19,13 @@ from pyrogram.errors import MessageNotModified
 from chatgpt.module.what import *
 from config import OPENAI_API 
 
-@ren.on_message(filters.command("ask") & filters.private | filters.group)
+CMD_HANDLER = ["!", "/"] # your change handler
+
+cmd = CMD_HANDLER
+
+@ren.on_message(filters.command(["ai", "ask"], cmd) & filters.private | filters.group)
 async def chatgpt(c: Client, m: Message):
-    randydev = (
-        m.text.split(None, 1)[1]
-        if len(
-            m.command,
-        )
-        != 1
-        else None
-    )
+    randydev = (m.text.split(None, 1)[1] if len(m.command) != 1 else None)
     if not randydev:
        await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
        return
@@ -50,7 +47,7 @@ async def chatgpt(c: Client, m: Message):
     try:
         response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
         await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
         await c.send_message(m.chat.id, response["choices"][0]["text"], reply_to_message_id=m.id)
     except Exception:
         await c.send_message(m.chat.id, "Yahh, sorry i can't get your answer.", reply_to_message_id=m.id)
