5522720a519254f0a958d4da67fc6742a8fe38c5
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-10 02:06:36 +0700

    [+] add library openai and library RyuzakiLib `OpenAiToken()`  `. message.output` and `.photo_output`

--- chatgpt/module/chatgpt.py
+++ chatgpt/module/chatgpt.py
@@ -7,20 +7,20 @@
 # All rights reserved. See COPYING, AUTHORS.
 #
 
-import requests
-import os
-import json
-import random
 import asyncio
+from pyrogram.types import Message
 from pyrogram import *
 from pyrogram.types import *
-from pyrogram import Client as ren 
-from pyrogram.errors import MessageNotModified
+from pyrogram import Client as ren
 from config import OPENAI_API 
 
-CMD_HANDLER = ["!", "/"] # your change handler
+from RyuzakiLib.hackertools.openai import OpenAiToken
+
+CMD_HANDLER = ["!", "/"]
 
 cmd = CMD_HANDLER
+
+openai_message = OpenAiToken(OPENAI_API)
 
 @ren.on_message(filters.command(["ai", "ask"], cmd) & filters.private | filters.group)
 async def chatgpt(c: Client, m: Message):
@@ -28,13 +28,32 @@ async def chatgpt(c: Client, m: Message):
     if not randydev:
        await m.reply(f"use command <code>/{m.command[0]} [question]</code> to ask questions using the API.")
        return
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {OPENAI_API}"}
-
-    json_data = {"prompt": randydev, "model": "text-davinci-003", "temperature": 0.5, "max_tokens": 1024, "n": 1, "stop": None, "top_p": 0.3, "frequency_penalty": 0.5}
     try:
-        response = requests.post("https://api.openai.com/v1/completions", headers=headers, json=json_data).json()
+        output_text = openai_message.message_output(randydev)
         await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
         await asyncio.sleep(2)
-        await c.send_message(m.chat.id, response["choices"][0]["text"], reply_to_message_id=m.id)
+        await c.send_message(
+            m.chat.id,
+            output_text,
+            reply_to_message_id=m.id
+        )
+    except Exception:
+        await c.send_message(m.chat.id, "Yahh, sorry i can't get your answer.", reply_to_message_id=m.id)
+
+@ren.on_message(filters.command(["dalle"], cmd) & filters.private | filters.group)
+async def dalle(c: Client, m: Message):
+    randydev = m.text.split(" ", 1)[1] if len(m.command) > 1 else None
+    if not randydev:
+       await m.reply(f"use command <code>/{m.command[0]} [question]</code> to dall e image generator using the API.")
+       return
+    try:
+        output_photo = openai_message.photo_output(randydev)
+        await c.send_chat_action(m.chat.id, enums.ChatAction.PHOTO)
+        await asyncio.sleep(2)
+        await c.send_photo(
+            m.chat.id,
+            output_photo,
+            reply_to_message_id=m.id
+        )
     except Exception:
         await c.send_message(m.chat.id, "Yahh, sorry i can't get your answer.", reply_to_message_id=m.id)
