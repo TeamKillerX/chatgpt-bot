a08b414ce611a026a278c852ef28fc73d2dfd6c6
xtsea <90479255+xtsea@users.noreply.github.com>
2023-11-04 16:32:55 +0700

    Update start

--- chatgpt/module/start.py
+++ chatgpt/module/start.py
@@ -10,11 +10,22 @@
 from pyrogram import Client as ren
 from pyrogram import *
 from pyrogram.types import *
+import database as db
 
 @ren.on_message(filters.command("start") & filters.private)
-async def start_bot(c: Client, m: Message):
+async def start_welcome(c: Client, m: Message):
     user_id = m.from_user.id
     first_name = m.from_user.first_name
     start_welcome = f"Hey {m.from_user.mention}\n\nWelcome to my bot! Send me a message and I will use the OpenAI API to generate a response.\n\nExample `/ask hello world`"
-    start_button = InlineKeyboardMarkup([[InlineKeyboardButton("ADD ME", url=f"https://t.me/{c.me.username}?startgroup=True")]])
+    start_button = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "ADD ME",
+                    url=f"https://t.me/{c.me.username}?startgroup=True"
+                )
+            ]
+        ]
+    )
+    db.start_bot(user_id)
     await m.reply_text(start_welcome, reply_markup=start_button)
