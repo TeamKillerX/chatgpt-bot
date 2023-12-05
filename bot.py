80cf0f98ad63b648d2594d00c019acb035a8739d
xtsea <90479255+xtsea@users.noreply.github.com>
2023-12-06 05:46:56 +0700

    _

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ from pyrogram import *
 from pyrogram.types import *
 from config import API_ID, API_HASH, BOT_TOKEN
 
-
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
@@ -22,7 +21,7 @@ if __name__ == "__main__":
 
 app = Client(
     name="app",
-    api_id=API_ID,
+    api_id=int(API_ID),
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=plugins,
