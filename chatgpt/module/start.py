9f9c37813ed34b0d11462c735f700ea70478975b
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-27 14:31:06 +0700

    _

--- chatgpt/module/start.py
+++ chatgpt/module/start.py
@@ -10,7 +10,6 @@
 from pyrogram import Client as ren
 from pyrogram import *
 from pyrogram.types import *
-import database as db
 
 @ren.on_message(filters.command("start") & filters.private)
 async def start_welcome(client: Client, message: Message):
@@ -19,14 +18,7 @@ Hey there {message.from_user.mention}
 
 • Welcome to my bot! Send me a message and I will use the Assistant Bot to generate a response.
 
-- Command: /ai question 
-- Command: /ai2 question
-- Command: /bingai question
-- Command: /jokeai question
-- Command: /dalle image generator
-- Command: /addkey setting api key
-
+- Command: /ask question 
 • Developer : t.me/xtdevs
 """
-    db.start_bot(user_id)
     await client.send_message(message.chat.id, MESSAGE, reply_to_message_id=message.id, disable_web_page_preview=True)
