0d19e2a27522cfa9a16e1ebe7caabfba91ee814e
xtsea <90479255+xtsea@users.noreply.github.com>
2024-07-27 14:32:44 +0700

    _

--- config.py
+++ config.py
@@ -5,6 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 API_ID = os.environ["API_ID"]
 API_HASH = os.environ["API_HASH"]
-ADMINS = os.environ["ADMINS"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-MONGO_URL = os.environ["MONGO_URL"]
